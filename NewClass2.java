c583a82126fe38652139eb02d89ce911836e70d3
Chaitra <blchaitra58@gmail.com>
2022-04-11 18:55:33 +0530

    commit

---
+++ NewClass2.java
@@ -0,0 +1,10 @@
+
+public class NewClass2 {
+    public static void main(String[] args)
+    {
+        int [] a=new int[4];
+        a[1]=1;
+        a=new int[2];
+        System.out.println("a[1] is" +a[1]);
+    }
+}

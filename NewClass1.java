5b6d51699d4d4a40bc8aa296f695168b60e93685
Chaitra <blchaitra58@gmail.com>
2022-04-11 18:55:02 +0530

    commit

---
+++ NewClass1.java
@@ -0,0 +1,10 @@
+class NewClass1 {
+    public static void main(String args[]) {
+        int array_variable [] = new int[10];
+        for (int i =9 ; i >= 0; i--) {
+            array_variable[i] = i;
+            System.out.print(array_variable[i] + " ");
+            i--;
+        }
+    }
+}

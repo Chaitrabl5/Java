8fe8d7d02bf10d01ffe5ef854621bf69e64e62d4
Chaitra <blchaitra58@gmail.com>
2022-04-11 18:50:33 +0530

    commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 10;
+        int c = b + a;
+        System.out.println("b = " + c);
+        System.out.println("c = " + b);
+        a = c + b;
+        System.out.println("a = " + a);
+    }
+}

b7f21d9c0657484c8e7d0e272aed3f9d430de1bb
Chaitra <blchaitra58@gmail.com>
2022-05-09 23:10:16 +0530

    commit

---
+++ New_Class2.java
@@ -0,0 +1,25 @@
+
+package onlinevideos;
+
+
+public class New_Class2
+{
+    public static void main(String[ ] args)
+    {    
+        
+        long h=100000000000000000l;//8 bytes//
+        float f=5.5f;
+       int i=5;//4 bytes->    32 bits-->-2,147,483,648 to 2,147,483,647
+       short  v=5;//2 bytes -> 16 bits range -32768 to 32767
+       byte e=5;//1 byte->8bits -128 to 127 
+       double d=5.5;
+       char c='A';
+       c=66;//ascii value----- american standard code for information interchange
+       System.out.println(c);
+       double d1=5;//implicit conversion as double stores point values also//
+       System.out.println(d1);
+       int k=(int)5.6;//explicit conversion//
+       System.out.println(k);
+       
+   }
+}

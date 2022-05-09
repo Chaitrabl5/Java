f8b6ba3a00e21060956e80fa72e00c186f14ac52
Chaitra <blchaitra58@gmail.com>
2022-05-09 23:09:25 +0530

    commit

---
+++ New_Class.java
@@ -0,0 +1,23 @@
+package onlinevideos;
+
+
+public class New_Class
+{
+    public static void main(String[ ] args)
+    {
+    int     i=7,  j=4;
+    int a,b,c,e;
+    double d;
+    a = i + j;//12//
+     b = i - j;//4//
+     c = i * j;//32//
+      d =(double) i / j;//2//explicit casting
+     e = i % j;//0//
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    
+}
+}

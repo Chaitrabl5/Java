ec48220b9771134d2e16d208ef757fd48ad5870b
Chaitra <blchaitra58@gmail.com>
2022-05-09 23:10:34 +0530

    commit

---
+++ New_Class3.java
@@ -0,0 +1,20 @@
+
+package onlinevideos;
+
+
+public class New_Class3
+{
+    public static void main(String[] args)
+    {
+       int n=8;
+        if(n%2==0)
+        {
+        System.out.println("even");
+        System.out.println("good morning");
+        }
+        if (n%2!=0)
+       System.out.println("odd");//else can be used//
+        if(n==0)
+            System.out.println("zero");
+            }
+}

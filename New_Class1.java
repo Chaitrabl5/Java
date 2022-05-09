6bf93c35535da24fb2ee130d0ac1681d6e7cfba7
Chaitra <blchaitra58@gmail.com>
2022-05-09 23:09:57 +0530

    commit

---
+++ New_Class1.java
@@ -0,0 +1,17 @@
+
+package onlinevideos;
+
+
+public class New_Class1
+{
+    public static void main(String[ ] args)
+    {
+        int $abc=1;//variable can be $ ,alphabet,number,underscore
+        int abc=2;//1st can be $ alpha or underscore but not number//
+        int    _af=0;
+        int fh4=9;
+        
+        System.out.print($abc);
+    }
+    
+}

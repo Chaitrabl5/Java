33cce6e486d3b1afdc0a2cca1ffb82c2603ec27f
Chaitra <blchaitra58@gmail.com>
2022-05-11 21:43:51 +0530

    commit

---
+++ New_Class6.java
@@ -0,0 +1,17 @@
+
+//iterative statements//to avoid code redundancy which is a crime in virtual world//
+
+public class New_Class6 {
+     public static void main(String[] args)// while loop//
+    {
+int i=1;//initialisation//
+while(i<=5 )//condition//if properly don't know the ending point//
+{
+          System.out.println("hello");
+          i++;
+}
+
+
+
+        }
+}

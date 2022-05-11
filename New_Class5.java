40800a2e7dbb15a64e1ae8e52b8ca8c59390a2f6
Chaitra <blchaitra58@gmail.com>
2022-05-11 21:43:28 +0530

    commit

---
+++ New_Class5.java
@@ -0,0 +1,21 @@
+
+package onlinevideos;
+
+
+public class New_Class5 {
+    public static void main(String[] args)//switch operator//
+    {
+        int n=3;
+        switch(n)
+        {
+            case 1:      System.out.println("one");
+                              break;
+            case 2:           System.out.println("two");
+                                   break;
+            case 3:       System.out.println("three");
+                                 break;
+            default:      System.out.println("no match");                 
+        }
+    }
+    
+}

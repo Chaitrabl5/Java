0793fe9d4896db2870f64f0ff7e9da7e85346965
Chaitra <blchaitra58@gmail.com>
2022-05-11 21:42:50 +0530

    commit

---
+++ New_Class4.java
@@ -0,0 +1,13 @@
+
+package onlinevideos;
+
+
+public class New_Class4 {
+    public static void main(String []args)//ternary operator//
+    {
+        int i=4,j;
+              j=  i>2?1:2;
+              System.out.println(j);
+    }
+    
+}

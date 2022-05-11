81afd00142034455e4c2de2872cd0b13878aa9c6
Chaitra <blchaitra58@gmail.com>
2022-05-11 21:44:14 +0530

    commit

---
+++ New_Class7.java
@@ -0,0 +1,21 @@
+//switch statement can use int values ,,char,,,string also for the source level above 1.7 ...........
+//it does not supports double //
+package onlinevideos;
+
+public class New_Class7 {
+ public static void main(String[] args)//switch operator//
+    {
+String n="ABC";
+ switch(n)
+        {
+            case "ABC":      System.out.println("one");
+                              break;
+            case "DCG":           System.out.println("two");
+                                   break;
+            case "BGH":       System.out.println("three");
+                                 break;
+            default:      System.out.println("no match");                 
+        }
+    }
+ }
+

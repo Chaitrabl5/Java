92a12844bfbdc100ef85dc1342188708a3c9a51a
Chaitra <blchaitra58@gmail.com>
2022-05-14 21:52:52 +0530

    commit

---
+++ NewClass15.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class NewClass15 {
+     
+  public static void main(String[]args) {
+    String[] elements = {"for","tea","too" };
+    String first = (elements.length>0)?elements[0]: null;
+    System.out.println(first);
+  }
+}
+

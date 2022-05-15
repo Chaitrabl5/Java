452e3b4edf01a76c7ed8ed20173854ad40cb28b4
Chaitra <blchaitra58@gmail.com>
2022-05-15 19:38:08 +0530

    commit

---
+++ NewClass16.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class NewClass16 {
+
+    public static void main(String[] args) {
+        double angle = 90;
+        double angleInRadians = Math.toRadians(angle);
+        double sineOfNum = Math.sin(angleInRadians);
+        System.out.println("sin("+angle+") = "+sineOfNum);
+    }
+}
+

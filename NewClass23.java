ff4e009ae930753e9b3e3b589d05abe4cc7eaad4
Chaitra <blchaitra58@gmail.com>
2022-05-17 23:13:42 +0530

    commit

---
+++ NewClass23.java
@@ -0,0 +1,18 @@
+public class NewClass23
+{ 
+    public static void main(String[] args)
+    {
+        try
+        {
+            return;
+        }
+
+        finally
+        {
+            System.out.println("Finally");
+        }
+    }
+}
+
+    
+

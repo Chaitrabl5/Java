3eeb13fea95b8ca40b430d875a40ff5842ea7c8e
Chaitra <blchaitra58@gmail.com>
2022-05-20 17:37:11 +0530

    commit

---
+++ NewClass30.java
@@ -0,0 +1,10 @@
+public class NewClass30 {
+    public static void main(String[] args) {
+        Thread t = new Thread();
+        System.out.println("Minimum = " + Thread.MIN_PRIORITY);
+        System.out.println("Maximum = " + Thread.MAX_PRIORITY);
+        System.out.println(t.getClass());
+    }
+
+    
+}
